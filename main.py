b8dd877c1f98784dc6a5b4b9772f311a683c6933
Michael Tracy <mtracy909@gmail.com>
2025-07-21 22:49:30 -0600

    comment changes

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ def main():
     raw = pd.read_csv("indexData.csv")
     proc = pd.read_csv("indexProcessed.csv", parse_dates=["Date"])
 
-
-    # print("Columns in indexProcessed.csv:")
-    # print(proc.columns.tolist())    
-    # Filter by the NASDAQ index code
     nasdaq = proc[proc["Index"] == "IXIC"].sort_values("Date")
     nasdaq.set_index("Date", inplace=True)
 
-    # Plot closing price
     nasdaq["Close"].plot(title="NASDAQ Composite Closing Price", figsize=(10, 5))
     plt.ylabel("USD")
     plt.show()
